c53a62594cf84ec3c1417aa7efb69078828e9bb5
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-15 14:59:22 +0500

    Update app.js

--- advance js/class 05/app.js
+++ advance js/class 05/app.js
@@ -3,7 +3,7 @@ crrlet arrobj = [
         p_id:1,  
         p_name:"Samsung A2",
         p_img_src:"https://images.priceoye.pk/samsung-galaxy-a2-core-pakistan-priceoye-jgy1e.jpg"   
-    },
+    },  
     {    
         p_id:2,
         p_name:"infinix note 7",
