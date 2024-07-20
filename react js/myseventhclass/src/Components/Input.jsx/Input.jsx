86dcaf124fb7e7fd8d72d3f2f767c230e680511c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-20 23:50:13 +0500

    Update Input.jsx

--- react js/myseventhclass/src/Components/Input.jsx/Input.jsx
+++ react js/myseventhclass/src/Components/Input.jsx/Input.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
    
 const Input = ({label,type,onchange,value}) => {   
-  return (
+  return (   
     <div>   
         <label htmlFor="">{label}</label>
         <input type={type} 
