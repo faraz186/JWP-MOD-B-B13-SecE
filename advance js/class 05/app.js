27c67891f39d013ab19b2ce51c715d019fbefec7
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-22 00:04:46 +0500

    Update app.js

--- advance js/class 05/app.js
+++ advance js/class 05/app.js
@@ -6,7 +6,7 @@ crrlet arrobj = [
     },  
     {    
         p_id:2,
-        p_name:"infinix note 7",
+        p_name:"infinix note 7",   
         p_img_src:"https://images.priceoye.pk/infinix-note-7-pakistan-priceoye-j870u.jpg"
     },
     {     
