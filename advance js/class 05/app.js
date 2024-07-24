b71a88626a2ee8e51c075709a0d2d12e3353a7b9
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-25 00:35:01 +0500

    Update app.js

--- advance js/class 05/app.js
+++ advance js/class 05/app.js
@@ -31,7 +31,7 @@ let div = document.getElementById('box');
 arrobj.map((e,i)=>{
     div.innerHTML += `<div class="card" style="width: 18rem;">
   <img src=${e.p_img_src} class="card-img-top" alt="...">
-  <div class="card-body">
+  <div class="card-body">   
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
