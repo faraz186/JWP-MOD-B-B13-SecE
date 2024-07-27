203833f49405ef5705d264c7d462dc08de45fef3
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-27 21:22:14 +0500

    react router dom start

---
+++ react js/myninthclass/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/Home" element={<h1>Home Page</h1>} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+      </Routes>
+
+    </>
+  );
+};
+
+export default App;
