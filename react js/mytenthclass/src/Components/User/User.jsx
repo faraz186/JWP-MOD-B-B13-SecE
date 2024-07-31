2b9e62db4abb2488104256ae15387e5f385eca9b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-07-31 23:14:40 +0500

    Update User.jsx

--- react js/mytenthclass/src/Components/User/User.jsx
+++ react js/mytenthclass/src/Components/User/User.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";   
 
 const User = () => {
-  const { key } = useParams();
+  const { key } = useParams();   
   console.log(key);
   return (    
     <div>
