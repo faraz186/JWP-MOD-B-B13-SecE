f00f609c7f73ab01ae36b55975b50f8cd373e0fd
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-01 00:12:38 +0500

    ink, NavLink & nested Routing

--- react js/mytenthclass/src/Screens/Home.jsx
+++ react js/mytenthclass/src/Screens/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 // import Navbar from "../Components/Navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";    
    
 const Home = () => {
   return (   
