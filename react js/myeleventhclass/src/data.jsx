40b840f686e0aa90ccbdb7b4f11d4eab2d3d99bf
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-04 13:30:09 +0500

    Update data.jsx

--- react js/myeleventhclass/src/data.jsx
+++ react js/myeleventhclass/src/data.jsx
@@ -5,7 +5,7 @@ export const productData = [
     price: 109.95,
     description:   
       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-    category: "men's clothing",
+    category: "men's clothing",   
     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",      
     rating: {   
       rate: 3.9,
