aad479f2f227bf6a02c9df8d786ed15c7170debd
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-07 23:56:51 +0500

    Update App.jsx

--- react js/mytwelvethclass/src/App.jsx
+++ react js/mytwelvethclass/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Screens/Home";
+import Home from "./Screens/Home";   
 import Products from "./Screens/Products";
 import Dashboard from "./Screens/Dashboard";   
 
