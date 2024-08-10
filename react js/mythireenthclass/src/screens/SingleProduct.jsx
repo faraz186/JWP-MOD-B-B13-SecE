a840136005dc16e514124e70ec1a68382898d78c
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-11 02:02:31 +0500

    Update SingleProduct.jsx

--- react js/mythireenthclass/src/screens/SingleProduct.jsx
+++ react js/mythireenthclass/src/screens/SingleProduct.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";   
 import { useParams } from "react-router-dom";   
 
-const SingleProduct = () => {
+const SingleProduct = () => {   
   const { id } = useParams();
    
   const [singleData, setSingleData] = useState([]);
