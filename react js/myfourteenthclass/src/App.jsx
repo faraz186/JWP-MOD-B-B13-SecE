dd9bed2cfc841933c1303445831693d49dcc05d1
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-14 22:47:18 +0500

    Update App.jsx

--- react js/myfourteenthclass/src/App.jsx
+++ react js/myfourteenthclass/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Home />} />   
       <Route path="createUser" element={<CreateUser />} />
-    </Routes>
+    </Routes>  
   );
 };
 
