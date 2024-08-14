5606bff165dae510cc20bade91b4a4f30c930f1f
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-14 22:47:06 +0500

    Update App.jsx

--- react js/myfourteenthclass/src/App.jsx
+++ react js/myfourteenthclass/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Screens/Home";
+import Home from "./Screens/Home";   
 import CreateUser from "./Screens/CreateUser";
 
 const App = () => {  
