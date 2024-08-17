d675fe9da8734454c88b42d6b7e02dfb365c6b4a
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-18 00:44:53 +0500

    Update Table.jsx

--- react js/myfifteenthclass/src/components/Table.jsx
+++ react js/myfifteenthclass/src/components/Table.jsx
@@ -16,7 +16,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
-  },   
+  },     
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
   },
