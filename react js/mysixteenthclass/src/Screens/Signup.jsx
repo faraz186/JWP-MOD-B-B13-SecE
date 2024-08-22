966140f03f1733b51657c4ffceb36af99ff3bdae
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-22 09:45:38 +0500

    Update Signup.jsx

--- react js/mysixteenthclass/src/Screens/Signup.jsx
+++ react js/mysixteenthclass/src/Screens/Signup.jsx
@@ -1,4 +1,4 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";  
 import React, { useState } from "react";
 import auth from "../config/firebase";    
 import { useNavigate } from "react-router-dom";
