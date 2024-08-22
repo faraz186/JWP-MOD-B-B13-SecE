dc2daae328bc466428628e3f32b7b4e55a48c2a1
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-22 09:45:21 +0500

    Update Signup.jsx

--- react js/mysixteenthclass/src/Screens/Signup.jsx
+++ react js/mysixteenthclass/src/Screens/Signup.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 const Signup = () => {
   const [name, setName] = useState("");
   const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");  
   const [password, setPassword] = useState("");  
   const navigate = useNavigate();
 
