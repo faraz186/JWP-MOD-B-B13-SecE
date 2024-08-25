8d939a06876e79c6fabc3cd66912e1b90ab8ba6b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-08-25 17:04:26 +0500

    Update App.jsx

--- react js/myseventeenthclass/src/App.jsx
+++ react js/myseventeenthclass/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <Route path="/dashboard" element={<Dashboard />} />
 
       <Route path="/signup" element={<Signup />} />
-    </Routes>
+    </Routes>   
   );   
 };
 
