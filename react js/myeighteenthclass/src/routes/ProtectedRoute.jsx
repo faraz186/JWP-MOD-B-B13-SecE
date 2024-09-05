2f99a088169dfa0c0e79f44ac576fcc6a1569f73
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-05 10:04:48 +0500

    Update ProtectedRoute.jsx

--- react js/myeighteenthclass/src/routes/ProtectedRoute.jsx
+++ react js/myeighteenthclass/src/routes/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";  
 
 const ProtectedRoute = () => {
-  return localStorage.getItem("userId") ? <Outlet /> : <Navigate to={"/"} />;   
+  return localStorage.getItem("userId") ? <Outlet /> : <Navigate to={"/"} />;     
 };
   
 export default ProtectedRoute;
