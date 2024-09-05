674256d483ecef5ff67a2c9ce0ca1ca45c0d0c1e
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-05 10:05:50 +0500

    login signup end to end connectivity

--- react js/myeighteenthclass/src/routes/AuthRoute.jsx
+++ react js/myeighteenthclass/src/routes/AuthRoute.jsx
@@ -4,6 +4,6 @@ import { Navigate, Outlet } from "react-router-dom";
 const AuthRoute = () => {  
   return !localStorage.getItem("userId") ? <Outlet /> : <Navigate to={"/home"} />;  
   
-};   
+};     
 
 export default AuthRoute;
