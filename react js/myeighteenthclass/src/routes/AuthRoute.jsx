45b77b0ca900b2b82a9500cf32fa392fdb99ab18
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-05 10:05:33 +0500

    Update AuthRoute.jsx

--- react js/myeighteenthclass/src/routes/AuthRoute.jsx
+++ react js/myeighteenthclass/src/routes/AuthRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";   
    
 const AuthRoute = () => {
-  return !localStorage.getItem("userId") ? <Outlet /> : <Navigate to={"/home"} />;
+  return !localStorage.getItem("userId") ? <Outlet /> : <Navigate to={"/home"} />;  
   
 };   
 
