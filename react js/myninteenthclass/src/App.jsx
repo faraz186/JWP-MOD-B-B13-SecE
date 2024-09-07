38f85c22b87fc91f5d28f6089f487983113b23ad
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-08 00:37:12 +0500

    Update App.jsx

--- react js/myninteenthclass/src/App.jsx
+++ react js/myninteenthclass/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>     
-        <Route path="/home" element={<CardSection />} />
+        <Route path="/home" element={<CardSection />} />  
         <Route path="/" element={<Login />} />
       </Routes>   
     </>   
