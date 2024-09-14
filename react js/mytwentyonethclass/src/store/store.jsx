8efb2b12f7ae755de8bf69615d058d685ce1a08e
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-14 16:38:59 +0500

    Update store.jsx

--- react js/mytwentyonethclass/src/store/store.jsx
+++ react js/mytwentyonethclass/src/store/store.jsx
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducers from './slices/counterSlice'
 import productReducers from './slices/productSlice'
 import addToCartReducer from './slices/addToCartSlice'  
-import { persistStore, persistReducer } from 'redux-persist'
+import { persistStore, persistReducer } from 'redux-persist'  
 import storage from 'redux-persist/lib/storage' 
 
 
