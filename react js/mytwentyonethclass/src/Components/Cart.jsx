3975dec43174990b2b69e2103e36c6bd93405781
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-19 01:07:14 +0500

    Update Cart.jsx

--- react js/mytwentyonethclass/src/Components/Cart.jsx
+++ react js/mytwentyonethclass/src/Components/Cart.jsx
@@ -1,6 +1,6 @@
 import { Grid } from '@mui/material';
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'   
 import Cards from './Card';
 import Header from './Header';
 
