bc6c2fea5706a9f4ab2a8a556caf7a1ae0dbb169
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-29 02:15:32 +0500

    Update App.jsx

--- react native/class 01/App.jsx
+++ react native/class 01/App.jsx
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'  
 
 export default function App() {    
-  return (  
+  return (    
     <View>
       <Text>Hello World</Text>    
     </View>    
