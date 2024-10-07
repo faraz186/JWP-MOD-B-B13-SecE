18beff0e3db09dad11168847f418073e4a8f27ae
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-08 00:25:15 +0500

    Update index.jsx

--- react native/class 03/config/App_Navigation/index.jsx
+++ react native/class 03/config/App_Navigation/index.jsx
@@ -1,5 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';  
 import Home from '../../Screens/Home';
 import About from '../../Screens/About';
 import Contact from '../../Screens/Contact';
