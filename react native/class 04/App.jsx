58235b790221b9c70c8ef27d43c91f2f48a90379
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-08 00:27:37 +0500

    Update App.jsx

--- react native/class 04/App.jsx
+++ react native/class 04/App.jsx
@@ -2,22 +2,11 @@ import React, { useEffect, useState } from 'react'
 import App_Navigation from './config/App_Navigation'
 
 const App = () => {
-  const [hide_splash_screen, setHide_splash_screen] = useState(false)
-  useEffect(() => {
-    setTimeout(() => {
-      setHide_splash_screen(true)
-    }, 3000);
-  }, [])
-  
-  // return hide_splash_screen ? <App_Navigation /> : 
-  //   <Splash_Screen /> 
    return <App_Navigation />
   
 }
 
 export default App
-
-
 
 // cd  android 
 //  gradlew clean
