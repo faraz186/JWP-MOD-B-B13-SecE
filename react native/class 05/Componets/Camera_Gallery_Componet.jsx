c6bdefd5d829394b39c795c9df533b7cf490a2b0
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-11 15:28:31 +0500

    Update Camera_Gallery_Componet.jsx

--- react native/class 05/Componets/Camera_Gallery_Componet.jsx
+++ react native/class 05/Componets/Camera_Gallery_Componet.jsx
@@ -8,7 +8,7 @@ const Image_Picker = () => {
   const open_Camera = async () => {  
     const result = await launchCamera({mediaType: 'photo', selectionLimit: 3});
     console.log(result);
-
+  
     if (result) {  
       const img_Src = result.assets[0].uri;  
       setCaptureImage(img_Src);
