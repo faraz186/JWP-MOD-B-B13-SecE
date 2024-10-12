33f563cf43d1d5190602fbacf8280370d5f99b46
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-12 23:10:28 +0500

    Update App.jsx

--- react native/class 05/App.jsx
+++ react native/class 05/App.jsx
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'  
 import { Text, TouchableOpacity } from 'react-native'  
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import AntDesign from 'react-native-vector-icons/AntDesign'  
 import { Async_Storage_Component } from './Componets/Async_Storage_Component'
 import { Camera_Gallery_Component } from './Componets/Camera_Gallery_Componet'    
 
