de1d9b2661ccd7d0876d98796109d54838845352
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-19 21:18:38 +0500

    Add files via upload

---
+++ backend/class01/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { data } from "./data.js";
+
+const app = express();
+
+// PORT
+
+app.get("/products", (request, response) => {
+  response.send(data);
+});
+
+app.get("/users", (request, response) => {
+  response.send({
+    name: "faraz",
+    email: "faraz@gmail.com",
+    phone: "1111136111",
+  });
+});
+
+app.get("/foods", (request, response) => {
+  response.send("foods data");
+});
+
+app.get("/client", (request, response) => {
+  response.send("client data");
+});
+
+app.get("/", (request, response) => {
+  response.send("server running on /");
+});
+
+app.listen(5000, () => {
+  console.log("SERVER RUNNING..");
+});
