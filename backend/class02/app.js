50408b6ca6ecb51c2ce481e1bfb792e9ab658930
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-10-24 00:32:39 +0500

    Update app.js

--- backend/class02/app.js
+++ backend/class02/app.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));   
 
 // mongo configuration
 
