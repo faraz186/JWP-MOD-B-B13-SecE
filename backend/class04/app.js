830d71318ba8e3c152ad8b7a2564cfcc670c84a7
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-03 01:12:43 +0500

    Update app.js

--- backend/class04/app.js
+++ backend/class04/app.js
@@ -4,7 +4,7 @@ import postModel from "./models/postSchema.js";
 import userModel from "./models/userSchema.js";    
 import bcrypt from "bcryptjs";
 import cors from "cors";  
-
+  
 const app = express();
 const PORT = 8080;     
 const DBURI = "";  
