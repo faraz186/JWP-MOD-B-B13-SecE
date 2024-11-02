11c120ee1c5f6b912c64aa9e67cf555c1951a64b
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-03 01:12:20 +0500

    Update app.js

--- backend/class04/app.js
+++ backend/class04/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
   
 app.use(cors());
 
-mongoose.connect(DBURI);  
+mongoose.connect(DBURI);    
 
 mongoose.connection.on("connected", () => console.log("mongodb connected"));
 
