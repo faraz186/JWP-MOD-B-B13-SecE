7bc16da7381b7bab3232cff979f068ac23e2ca7e
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-04 18:23:35 +0500

    Update app.js

--- backend/class05/app.js
+++ backend/class05/app.js
@@ -1,7 +1,7 @@
 import "dotenv/config";  
 import express from "express";  
 import mongoose from "mongoose";
-import postModel from "./models/postSchema.js";  
+import postModel from "./models/postSchema.js";    
 import userModel from "./models/userSchema.js";       
 import bcrypt from "bcryptjs";    
 import cors from "cors";    
