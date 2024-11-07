77fc574012e17ce0e26959d9ed9ac54e7febc974
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-07 09:42:52 +0500

    Update app.js

--- backend/class06/backend/app.js
+++ backend/class06/backend/app.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import postModel from "./models/postSchema.js";
 import userModel from "./models/userSchema.js";
 import bcrypt from "bcryptjs";
-import cors from "cors";
+import cors from "cors";  
 import jwt from "jsonwebtoken";
 import userVerifyMiddle from "./middleware/userVerify.js";  
 
