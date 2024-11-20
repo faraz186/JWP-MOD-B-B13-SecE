07ddea9ff905eb061be0b4adda0ecc1db5994fdf
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-20 20:55:45 +0500

    Add files via upload

---
+++ Typescript/typescript/index.js
@@ -0,0 +1,37 @@
+"use strict";
+// console.log("Learning Typescript!!");
+// var num = 10;
+// console.log(num);
+// var firstName: string = "faraz";
+// var percentage: number = 85;
+// console.log(firstName, percentage);
+// var bioData: number | string | boolean = 41;
+// var abc: number | string = "faraz";
+// abc = "true";
+// var age: string | number = 20;
+// if (age === 20) {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+// var arr: (string | number | boolean)[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// arr.push("faraz");
+// arr.unshift(true);
+// console.log(arr);
+// Function
+function abc(name, num) {
+    console.log(name + num);
+}
+abc("faraz");
+let obj1 = {
+    name: "faraz",
+    age: 24,
+    email: "faraz@gmail.com",
+    id: 5455,
+};
+let obj2 = {
+    className: "WAM MOD-B",
+    studentdata: obj1,
+};
+console.log("obj1 ", obj1);
+console.log("obj2", obj2);
---
+++ Typescript/typescript/index.ts
@@ -0,0 +1,76 @@
+// console.log("Learning Typescript!!");
+
+// var num = 10;
+
+// console.log(num);
+
+// var firstName: string = "faraz";
+
+// var percentage: number = 85;
+
+// console.log(firstName, percentage);
+
+// var bioData: number | string | boolean = 41;
+
+// var abc: number | string = "faraz";
+
+// abc = "true";
+
+// var age: string | number = 20;
+
+// if (age === 20) {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+
+// var arr: (string | number | boolean)[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// arr.push("faraz");
+
+// arr.unshift(true);
+
+// console.log(arr);
+
+// Function
+
+function abc(name: string, num?: number) {
+  console.log(name + num);
+}
+
+abc("faraz");
+
+// Object
+
+// object type casting
+
+// interface
+
+interface Student {
+  name: string;
+  age: number;
+  email: string;
+  id?: number;
+  status?: boolean;
+}
+
+let obj1: Student = {
+  name: "faraz",
+  age: 24,
+  email: "faraz@gmail.com",
+  id: 5455,
+};
+
+interface Student1 {
+  className: string;
+  studentdata: Student;
+}
+
+let obj2: Student1 = {
+  className: "WAM MOD-B",
+  studentdata: obj1,
+};
+
+console.log("obj1 ", obj1);
+
+console.log("obj2", obj2);
