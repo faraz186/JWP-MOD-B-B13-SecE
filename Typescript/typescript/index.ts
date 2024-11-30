54379deb52d387b9d459d77ce7e0d21f74436dc8
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-11-30 16:38:41 +0500

    Update index.ts

--- Typescript/typescript/index.ts
+++ Typescript/typescript/index.ts
@@ -12,7 +12,7 @@ console.log(firstName, percentage);
 
 var bioData: number | string | boolean = 41;     
   
-var abc: number | string = "faraz";
+var abc: number | string = "faraz";  
   
 abc = "true";
 
