1e26c01e08d700d08bac9afa45bd38de8acb726d
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2025-07-12 16:12:06 +0500

    Update AutoCompleteMUI.jsx

--- react js/myeighthclass/src/components/AutoCompleteMUI.jsx
+++ react js/myeighthclass/src/components/AutoCompleteMUI.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";  
 import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import Autocomplete from "@mui/material/Autocomplete"; 
 
 export default function ComboBox() {
   return (   
